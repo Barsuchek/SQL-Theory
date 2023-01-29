a684510a539453211d032418370f3e9bd90c0f68
Barsuchek <rakbarcik@yandex.ru>
2023-01-29 21:51:28 +0300

    Update INSERT.sql

--- ProjectsForTraining/Topic3/INSERT.sql
+++ ProjectsForTraining/Topic3/INSERT.sql
@@ -1,4 +1,7 @@
 USE GitHub_Theory;
 
-INSERT INTO Test (ID, Names, Other) VALUES ('1', 'Ivan', 'Любит отдыхать на море'); --Добавление записи в таблицу Test
+INSERT INTO Test (ID, Names, Other)          --Добавление записи в таблицу Test
+	VALUES 
+	('1', 'Ivan', 'Любит отдыхать на море'),
+	('2','','Не любит мороженое');
 
