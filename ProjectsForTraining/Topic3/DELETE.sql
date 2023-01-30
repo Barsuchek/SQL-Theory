eda78abea8c0b9f53fdd3a9abf0077afc4a3c169
Barsuchek <rakbarcik@yandex.ru>
2023-01-30 22:45:39 +0300

    Create DELETE.sql
    Добавление нового материала для тренировки

---
+++ ProjectsForTraining/Topic3/DELETE.sql
@@ -0,0 +1,7 @@
+USE GitHub_Theory;
+
+DELETE FROM Test WHERE ID > 4; --Удалить строки данных из таблицы Test, где ID > 4
+
+DELETE FROM Test; --Удалить все строки данных из таблицы Test
+
+TRUNCATE TABLE Test; --Удалить все строки данных из таблицы Test
