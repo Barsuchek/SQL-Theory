3576760b5acd70711f24bf1660db6b7e3dec9b58
Barsuchek <rakbarcik@yandex.ru>
2023-01-31 00:33:37 +0300

    Create LIMIT.sql

---
+++ ProjectsForTraining/Topic4/LIMIT.sql
@@ -0,0 +1,5 @@
+USE GitHub_Theory;
+
+SELECT * FROM Test LIMIT 3; --Выбор всех полей из таблицы Test и вывод 3 строк
+
+SELECT * FROM Test LIMIT 2, 3; --Выбор всех полей из таблицы Test, пропустив две строки и выведя 3 строки
