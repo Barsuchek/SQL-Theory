1f51d82c6714765e55786f427d69b325dfdd5b2b
Barsuchek <rakbarcik@yandex.ru>
2023-01-31 01:06:43 +0300

    Update LIKE.sql
    Добавление информации в проект для тренировки

--- ProjectsForTraining/Topic4/LIKE.sql
+++ ProjectsForTraining/Topic4/LIKE.sql
@@ -1,3 +1,5 @@
 USE GitHub_Theory
 
 SELECT * FROM Test WHERE Other LIKE 'Не%'; --Выбор всех полей из таблицы Test и вывод значений с началом Не
+
+SELECT * FROM Test WHERE Other LIKE '%во%'; --Выбор всех полей из таблицы Test и вывод значений, в которых есть во
