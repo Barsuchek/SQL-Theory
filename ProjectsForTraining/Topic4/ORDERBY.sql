3196784b7acde79bd4244e69d0d020277bf3d0bc
Barsuchek <rakbarcik@yandex.ru>
2023-01-31 00:35:25 +0300

    Create ORDERBY.sql

---
+++ ProjectsForTraining/Topic4/ORDERBY.sql
@@ -0,0 +1,9 @@
+USE GitHub_Theory
+
+SELECT * FROM Test ORDER BY Names; --¬ыбрать все пол€ из таблицы Test и отсортировать их по-возрастанию(по алфавиту)
+
+SELECT * FROM Test ORDER BY ID; --¬ыбрать все пол€ из таблицы Test и отсортировать их по-возрастанию
+
+SELECT * FROM Test ORDER BY Names DESC; --¬ыбрать все пол€ из таблицы Test и отсортировать их по-убыванию(по алфавиту)
+
+SELECT * FROM Test ORDER BY ID DESC; --¬ыбрать все пол€ из таблицы Test и отсортировать их по-убыванию
