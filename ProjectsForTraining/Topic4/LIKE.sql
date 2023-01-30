0335d8199c16511f4a214d1c9f5309c4af3d41d4
Barsuchek <rakbarcik@yandex.ru>
2023-01-31 01:11:59 +0300

    Update LIKE.sql
    Добавление информации в проект для тренировки

--- ProjectsForTraining/Topic4/LIKE.sql
+++ ProjectsForTraining/Topic4/LIKE.sql
@@ -3,3 +3,5 @@ USE GitHub_Theory
 SELECT * FROM Test WHERE Other LIKE 'Ќе%'; --¬ыбор всех полей из таблицы Test и вывод значений с началом Ќе
 
 SELECT * FROM Test WHERE Other LIKE '%во%'; --¬ыбор всех полей из таблицы Test и вывод значений, в которых есть во
+
+SELECT * FROM Test WHERE Other LIKE '%ь'; --¬ыбор всех полей из таблицы Test и вывод значений, которые заканчиваютс€ на ь
