c75a1e6b931c260ec9c67ffdb679734620c0c393
Barsuchek <rakbarcik@yandex.ru>
2023-02-03 00:22:35 +0300

    Create INDEX.sql
    Добавление проекта для тренировки

---
+++ ProjectsForTraining/Topic5/INDEX.sql
@@ -0,0 +1,5 @@
+USE GitHub_Theory;
+
+CREATE INDEX NIndex ON Test(Names); --Создать индекс с названием NIndex в таблице Test, в поле Names
+
+DROP INDEX NIndex ON Test; --Удалить индекс NIndex в таблице Test
