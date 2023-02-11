322979f012c20ca1c0eb6eb7dae0b664b8cf5377
Barsuchek <rakbarcik@yandex.ru>
2023-02-11 20:22:00 +0300

    Update Псевдонимы.sql
    Добавление информации для тренировки

--- ProjectsForTraining/Topic8/Псевдонимы.sql
+++ ProjectsForTraining/Topic8/Псевдонимы.sql
@@ -1,3 +1,5 @@
 USE GitHub_Theory;
 
-SELECT Names AS 'Имя', Other AS 'Описание' FROM Person;
+SELECT Names AS 'Имя', Other AS 'Описание' FROM Person; --Вывести поле Names с псевдонимом Имя и поле Other с псевдонимом Описание из таблицы Person
+
+SELECT P.Names, S.Title FROM Person AS P, Shop AS S; --Вывести поле Names из таблицы Person и поле Title из таблицы Shop, выводя данные из таблицы Person с псевдонимом P и данные из таблицы Shop с псевдонимом S
