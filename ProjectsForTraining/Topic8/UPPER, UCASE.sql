b45a6606285e396fadf83f7694045b903e060ec4
Barsuchek <rakbarcik@yandex.ru>
2023-02-11 21:17:36 +0300

    Create UPPER, UCASE.sql
    Добавлений файла для тренировок

---
+++ ProjectsForTraining/Topic8/UPPER, UCASE.sql
@@ -0,0 +1,7 @@
+USE GitHub_Theory
+
+--MSSQL
+SELECT UPPER(Title) FROM Shop; --Вывести значения в верхнем регистре из поля Title из таблицы Shop
+
+--MySQL
+SELECT UCASE(Title) FROM Shop; --Вывести значения в верхнем регистре из поля Title из таблицы Shop
