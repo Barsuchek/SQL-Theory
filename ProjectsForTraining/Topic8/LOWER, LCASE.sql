4c054632bbb538dfcbd9df07c2974a73e167cb17
Barsuchek <rakbarcik@yandex.ru>
2023-02-11 21:17:46 +0300

    Create LOWER, LCASE.sql
    Добавлений файла для тренировок

---
+++ ProjectsForTraining/Topic8/LOWER, LCASE.sql
@@ -0,0 +1,7 @@
+USE GitHub_Theory
+
+--MSSQL
+SELECT LOWER(Title) FROM Shop; --Вывести значения в нижнем регистре из поля Title из таблицы Shop
+
+--MySQL
+SELECT LCASE(Title) FROM Shop; --Вывести значения в нижнем регистре из поля Title из таблицы Shop
